ad2200b7e58c5107d56706ec28847ae78f68b5ea
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-10-21 21:53:27 +0300

    v2 add ';'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const numberA = prompt("Enter the number a");
 const numberB = prompt("Enter the number b");
 const numberC = prompt("Enter the number c");
-const result = ((Number(numberA) + Number(numberB) + Number(numberC)) / 3)
+const result = ((Number(numberA) + Number(numberB) + Number(numberC)) / 3);
 alert("(a+b+c)/3= " + result)
