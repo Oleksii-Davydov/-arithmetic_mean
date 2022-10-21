2f82d7b229f029a41dc0b383d70954d0a61461fd
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-10-21 21:50:19 +0300

    v2

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const a =prompt("Enter the number a");
-const b =prompt("Enter the number b");
-const c =prompt("Enter the number c");
-const n = +3
-
-const result = ((Number(a)+Number(b)+Number(c))/n)
-alert("(a+b+c)/3: "+result)
+const numberA = prompt("Enter the number a");
+const numberB = prompt("Enter the number b");
+const numberC = prompt("Enter the number c");
+const result = ((Number(numberA) + Number(numberB) + Number(numberC)) / 3)
+alert("(a+b+c)/3= " + result)
